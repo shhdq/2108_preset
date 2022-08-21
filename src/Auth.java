f6cf754f386cc03ab1d1e335d501ec74302b6be2
shhdq <54862478+shhdq@users.noreply.github.com>
2022-08-21 13:24:45 +0300

    All steps

--- src/Auth.java
+++ src/Auth.java
@@ -57,8 +57,70 @@ public class Auth implements ActionListener {
         frame.setVisible(true);
     }
 
+    public String md5(String value) throws Exception {
+        MessageDigest algo = MessageDigest.getInstance("MD5");
+
+        // manaparole
+        algo.update(value.getBytes(), 0, value.length());
+
+        return new BigInteger(1, algo.digest()).toString(16);
+    }
+
     public void actionPerformed(ActionEvent e) {
 
+        String userVal = userInput.getText();
+        String passVal = userPass.getText();
+
+        DBlogic db = new DBlogic();
+
+        if(e.getSource() == register) {
+            if(userVal.length() == 0 || passVal.length() == 0) {
+                System.out.println("Empty field");
+            } else {
+
+                boolean check = Boolean.parseBoolean(db.select(userVal));
+
+                if(check) {
+                    System.out.println("User already exists");
+                } else {
+                    String md5 = null;
+
+                    try {
+                        md5 = md5(passVal);
+                    } catch (Exception a) {
+                        a.printStackTrace();
+                    }
+
+                    System.out.println(md5);
+                    db.register(userVal, md5);
+                }
+            }
+        } else if(e.getSource() == login) {
+            if(userVal.length() == 0 || passVal.length() == 0) {
+                System.out.println("Empty fields");
+            } else {
+                String md5 = null;
+
+                try {
+                    md5 = md5(passVal);
+                } catch (Exception a) {
+                    a.printStackTrace();
+                }
+
+                boolean ch = db.login(userVal, md5);
+
+                if(ch) {
+                    frame.remove(panel);
+                    frame.add(dashboard);
+                    frame.repaint();
+                    frame.setTitle("Welcome " + userVal);
+//                    System.out.println("Works");
+                }
+
+            }
+        } else {
+            System.out.println("Error");
+        }
 
     }
 
--- src/DBlogic.java
+++ src/DBlogic.java
@@ -1,18 +1,85 @@
+import java.sql.*;
+
 public class DBlogic {
     String DB = "jdbc:mysql://localhost:3306/2108";
     String USER = "root";
     String PASS = "biskviits";
 
     // register
-    public void register() {
+    public void register(String username, String password) {
+        try {
 
+            Connection conn = DriverManager.getConnection(DB, USER, PASS);
+
+            String insert = "INSERT INTO users (username, password) VALUES (?, ?)";
+
+            PreparedStatement ps = conn.prepareStatement(insert);
+
+            ps.setString(1, username);
+            ps.setString(2, password);
+
+            ps.executeUpdate();
+
+            conn.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     // get user
-    public String select() {
+    public String select(String username) {
+        try {
+            Connection conn = DriverManager.getConnection(DB, USER, PASS);
 
+            String select = "SELECT username FROM users WHERE username = ?";
+
+            PreparedStatement ps = conn.prepareStatement(select);
+
+            ps.setString(1, username);
+
+            ResultSet rs = ps.executeQuery();
+
+            if(rs.next()) {
+                System.out.println("User already exists");
+            }
+
+            conn.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return username;
     }
 
     // login
-    public boolean login() {}
+    public boolean login(String username, String password) {
+        boolean isLoggedIn = false;
+
+        try {
+            Connection conn = DriverManager.getConnection(DB, USER, PASS);
+
+            String select = "SELECT username, password FROM users WHERE username = ? AND password = ?";
+
+            PreparedStatement ps = conn.prepareStatement(select);
+
+            ps.setString(1, username);
+            ps.setString(2, password);
+
+            ResultSet rs = ps.executeQuery();
+
+            if(rs.next()) {
+                isLoggedIn = true;
+            } else {
+                System.out.println("User not faund");
+            }
+
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return isLoggedIn;
+    }
+
 }
